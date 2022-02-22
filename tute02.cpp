c9ecf23d2307cd0a7e197f832a8e9028516d7c52
IT21249570 <91739119+sachinijayasundara@users.noreply.github.com>
2022-02-22 18:05:25 +0000

    Tutorial 02

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,40 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+#include <iostream>
+
+
 int main()
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
-
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+  int etype,otHrs,otRate;
+  double salary,netSalary;
   
-   return 0;
-}
+  std::cout<<"Enter Employee type :";
+  std::cin>>etype;
+
+  std::cout<<"Enter Salary :";
+  std::cin>>salary;
+
+  std::cout<<"Enter otHrs :";
+  std::cin>>otHrs;
+
+  switch (etype)
+    {
+      case 1:
+      otRate=1000;
+      break;
+    
+  case 2:
+    otRate=1500;
+    break;
+    default :
+      otRate=1700;
+      break;
+      }
+   
+
+netSalary=salary+(otHrs*otRate);
+std::cout<<"Net salary is :"<<netSalary;
+
+  return 0;
+  }
--- tute04.cpp
+++ tute04.cpp
@@ -18,12 +18,29 @@ long nCr(int n, int r);
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
+  std::cout << "Enter a value for n : ";
   std::cin >> n;
-  std::cout << "Enter a value for r ";
+  std::cout << "Enter a value for r :";
   std::cin >> r;
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
+  
   return 0;
-}
+  }
+
+  long Factorial(int no)
+  {
+   long fac=1;
+    for(int i=no;i>=1;i--){
+      fac=fac*i;
+      
+    }
+    return fac;
+  }
+  long nCr(int n, int r)
+  {
+    return 
+   Factorial(n)/(Factorial(r)*Factorial(n-r));
+      }
+
